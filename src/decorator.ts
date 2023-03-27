58e0744789988c57c35d64bb8f3c0a62e7ac66d1
Bharath Sriraam R R <albharath.1305@gmail.com>
2023-03-28 00:19:37 +0530

    This handles arbitrary nesting. However...
    I think a parser would be better but not
    sure about how performant it would be

--- src/decorator.ts
+++ src/decorator.ts
@@ -11,9 +11,7 @@ export class Decorator {
   unfoldIfLineSelected: boolean = false;
   supportedLanguages: string[] = [];
 
-  // regEx = /(class|className)=(['"`]|{(['"`]))(.*?)(\2|\3})/g
-
-  regEx = /(sx)=({(?:[^{}]*{[^{}]*}[^{}]*)*})/gs;
+  regEx = /(sx)=({(?:[^{}]+|{(?:[^{}]+|{(?:[^{}]+|{[^{}]*})*})+})*})/gs;
   regExGroup = 0;
 
   unfoldedDecorationType = UnfoldedDecorationType();
@@ -69,19 +67,31 @@ export class Decorator {
 
     let match;
     while ((match = this.regEx.exec(documentText))) {
-      if (match && !match[this.regExGroup]) {
+      if (match && !match[2]) {
         continue;
       }
 
+      // const text = match[0];
+      // const textToFold = match[2];
+      // const foldStartIndex = text.indexOf(textToFold);
+
+      // const foldStartPosition = this.activeEditor.document.positionAt(
+      //   match.index + foldStartIndex
+      // );
+      // const foldEndPosition = this.activeEditor.document.positionAt(
+      //   match.index + foldStartIndex + textToFold.length
+      // );
       const text = match[0];
-      const textToFold = match[this.regExGroup];
+      const textToFold = match[2];
+      console.log("match", match);
       const foldStartIndex = text.indexOf(textToFold);
+      const foldEndIndex = foldStartIndex + textToFold.length;
 
       const foldStartPosition = this.activeEditor.document.positionAt(
-        match.index + foldStartIndex
+        match.index + foldStartIndex - 1 // adjust for '{' before 'sx'
       );
       const foldEndPosition = this.activeEditor.document.positionAt(
-        match.index + foldStartIndex + textToFold.length
+        match.index + foldEndIndex // no need to adjust for closing '}' of the value
       );
       const range = new Range(foldStartPosition, foldEndPosition);
 
